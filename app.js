384b483b155a74e7e09b6d173d5d065f6b909ee0
kauayu <kaua.palmiro.ferreira@escola.pr.gov.br>
2025-10-17 10:56:43 -0300

    .

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let respostaEstaVisivel = false;
+
+function viraCartao() {
+    respostaEstaVisivel = !respostaEstaVisivel
+    viraCartao.classList.toggle('active', respostaEstaVisivel)
+}
+viraCartao.addEventListener('click', viraCartao)
+
+container.appendhild(catao)
+}
